29de4d8998c45ef0ff056435bf064cc74e08799c
n30w <rocketneo@gmail.com>
2023-02-14 10:33:29 -1000

    rearranged query.go

--- internal/models/query.go
+++ internal/models/query.go
@@ -1,9 +1,10 @@
 package models
 
+type verb uint8
+type amount uint8
+
 // Verb is a type of SQL verb.
 type Verb verb
-
-type verb uint8
 
 const (
 	Add Verb = iota
@@ -12,8 +13,6 @@ const (
 
 // Amount represents an amount of objects that are queried to be retrieved.
 type Amount amount
-
-type amount uint8
 
 const (
 	All Amount = iota
